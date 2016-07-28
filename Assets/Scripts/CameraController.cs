50b3d36b9cba35702eef36f6f1a1c2f296a3396b
Celfi <Celfi>
2016-07-27 23:04:11 -0400

    Title: Added plane, ball and a cube to the game. Developer: Celso

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject ball;
+	private Vector3 offset;
+
+	private Vector3 ballPosition;
+	private Vector3 cameraPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+		// calculate offset, based on first position of the camera and the ball.
+		ballPosition = ball.GetComponent<Transform> ().position;
+		cameraPosition = this.GetComponent<Transform> ().position;
+
+		offset = ballPosition - cameraPosition;
+	}
+	
+	// Garanteed that will excute after all items processed by Update
+	void LateUpdate () {
+	
+		// get current position of the ball
+		ballPosition = ball.GetComponent<Transform> ().position;
+
+		// calculate the next position of the camera. It will follow the ball.
+		cameraPosition = ballPosition - offset;
+		this.GetComponent<Transform> ().position = cameraPosition;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float forceAdition;
+	private Rigidbody rigid;
+
+	void Start ()
+	{
+		rigid = GetComponent<Rigidbody> ();
+	}
+
+	// Called before any physics calculations
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal"); 
+
+
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+
+		rigid.AddForce (movement * forceAdition);
+	}
+
+	// Update is called before rendering a frame.
+}
+
